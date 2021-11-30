8898888a5a3ee3231fb7388a385a53c82112c1e6
farizmilzam32 <fariz.milzamh17@gmail.com>
2021-11-30 14:08:51 +0700

    commit

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\KeranjangController;
+use App\Models\Keranjang;
 use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -18,6 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/products', function () {
     return Product::all();
 });
+
+Route::post('/keranjangs', [KeranjangController::class, 'store']);
+
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
